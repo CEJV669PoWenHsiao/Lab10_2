18507f96d338d67d9d8b24c22cbd3b77de36329d
Po-Wen Hsiao <p_hsiao@concordia.ca>
2019-03-07 19:07:48 -0500

    Initial commit

---
+++ app/src/main/java/cejv669/concordia/com/lab10_2/BroadcasterFragment.java
@@ -0,0 +1,86 @@
+package cejv669.concordia.com.lab10_2;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class BroadcasterFragment extends Fragment {
+
+
+
+    private OnFragmentInteractionListener mListener;
+
+    public BroadcasterFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_broadcaster, container, false);
+
+        Button b = v.findViewById(R.id.btnSend);
+
+        final EditText et = v.findViewById(R.id.txtSend);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = et.getText().toString();
+
+                mListener.messageFromBroadcastFragment(message);
+            }
+        });
+
+        // Inflate the layout for this fragment
+        return v;
+    }
+
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+
+        void messageFromBroadcastFragment(String text);
+    }
+}
---
+++ app/src/main/java/cejv669/concordia/com/lab10_2/ReceiverFragment.java
@@ -0,0 +1,45 @@
+package cejv669.concordia.com.lab10_2;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class ReceiverFragment extends Fragment {
+
+    TextView myTextView;
+
+
+    public ReceiverFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_receiver, container, false);
+
+        myTextView =  v.findViewById(R.id.txtReceiver);
+
+        // Inflate the layout for this fragment
+        return v;
+    }
+
+    public void  displayText(String text)
+    {
+        myTextView.setText(text);
+    }
+
+}
